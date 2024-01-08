2678f689193d39bf9968bacf0888d476156276fa
Yaroslavik <yaroslav4167@mail.ru>
2024-01-08 15:51:13 +0200

    Migrate to logixy.net

--- lw-proxy.js
+++ lw-proxy.js
@@ -9,7 +9,7 @@ const proxyList = []
 
 async function loadServerList() {
     let srvList = new Array;
-    await axios.get("https://logicworld.ru/monAJAX/ajax.php").then(res => {
+    await axios.get("https://logixy.net/monAJAX/ajax.php").then(res => {
         let data = res.data.servers
         let sortData = Object.values(data);
         for (let i = 0; i < sortData.length; i++) {
@@ -25,17 +25,16 @@ async function loadServerList() {
 }
 
 function startProxy() {
-    // proxy.createProxy(3939, "s2.logicworld.ru", 3939)
     loadServerList().then(data => {
         for (let i = 0; i < data.length; i++) {
             proxyList.push(proxy.createProxy(data[i].port, data[i].address, data[i].port)) ;
             console.log(`Proxy for ${data[i].address}:${data[i].port} started`);
         }
-        proxyList.push(proxy.createProxy(9274, "s2.logicworld.ru", 9274)) ; //LServer
+        proxyList.push(proxy.createProxy(9274, "s2.logixy.net", 9274)) ; //LServer
         console.log("Proxy for launc-server started");
-        proxyList.push(proxy.createProxy(9692, "play.logicworld.ru", 9692)); //Skins/Cloaks
+        proxyList.push(proxy.createProxy(9692, "play.logixy.net", 9692)); //Skins/Cloaks
         console.log("Proxy for skins and cloaks started");
-        proxyList.push(proxy.createProxy(8000, "play.logicworld.ru", 8000)); //RRRRRRRRADIO!
+        proxyList.push(proxy.createProxy(8000, "play.logixy.net", 8000)); //RRRRRRRRADIO!
         console.log("Proxy for RRRRRRRRADIO started");
         console.log(`Successfuly started`);
     }).catch(err => {
